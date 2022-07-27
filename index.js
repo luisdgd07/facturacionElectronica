a826760740b1673908fdf43553c6de7266869772
Luis Gonzalez <luisdgd07@gmail.com>
2022-07-27 14:02:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -334,7 +334,7 @@ app.post("/consultaruc", async (req, res) => {
     res.send("Ocurrio un error: ");
   }
 });
-app.listen(3000, () => {
+app.listen(80, () => {
   console.log("Servidor corriendo en puerto 3000");
 });
 // app.listen(3000, "127.0.0.1", () => {
